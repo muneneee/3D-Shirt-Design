6857d3de21ee9d9988703932d8dcd5599d951275
muneneee <lukekevin39@gmail.com>
2023-04-27 13:38:19 +0300

    setup app states

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
 
 const Home = () => {
   return (
