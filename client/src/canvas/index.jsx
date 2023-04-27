779b671a8465352b9ccb80c26a0b63cab91ae5bc
muneneee <lukekevin39@gmail.com>
2023-04-27 15:21:23 +0300

    setup canvas models

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
 
-const Canvas = () => {
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+const CanvasModel = () => {
   return (
     <div>Canvas</div>
   )
 }
 
-export default Canvas
+export default CanvasModel
