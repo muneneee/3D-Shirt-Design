3ea684d3f86a98f32a70190030cd3b579bda30d2
muneneee <lukekevin39@gmail.com>
2023-04-27 15:05:21 +0300

    setup customizer page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -8,10 +8,36 @@ import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key='custom'
+            className='absolute top=0 left-o z-10'
+            {...slideAnimation('left')}
+          >
+            <div className='flex items-center min-h-screen'>
+              <div className='editortabs-container tabs'>
+                {EditorTabs.map((tab) => (
+                  <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
+            </div>
+
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
